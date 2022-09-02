c8a83a414a9e2a0a26f1ec490b6575cc7c521ab6
Pawan Shrestha <pawanshresthait@gmail.com>
2022-09-02 11:12:35 +0545

    Create controller and routes for tasks

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 
+const taskRouter = require('./routes/tasks');
+
 const app = express();
+
+// routes
+app.get('/', (req, res, next) => {
+  res.redirect('/hello');
+});
+
+app.get('/hello', (req, res, next) => {
+  res.send('TASK MANAGER APP');
+});
+
+app.use('/api/v1/tasks', taskRouter);
 
 const PORT = 3000;
 
---
+++ controllers/tasks.js
@@ -0,0 +1,3 @@
+exports.getAllTask = (req, res, next) => {
+  res.send('GET ALL TASKS');
+};
